1f8805956f5e26b0450e7896e32ebca34510fde9
dubiefe <emiliedubief@free.fr>
2025-11-01 20:33:21 +0100

    Plants details component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Map from './map_component/map'
+import Plant_Details from './plant_details/plant_details'
 
 function App() {
 
   return (
     <>
-      <Map/>
+      <Plant_Details/>
     </>
   )
 }
--- frontend/src/plant_details/plant_details.jsx
+++ frontend/src/plant_details/plant_details.jsx
@@ -1,10 +1,24 @@
 import './plant_details.css'
+import default_plant_picture from "../assets/default_plant.jpg"
 
 function Plant_Details(props) {
 
     return (
         <>
-            
+            <div id='plant_container'>
+                <div id='plant_detail_first_layer_container'>
+                    <img src={default_plant_picture} alt="default_plant"/>
+                    <div id='plant_details_main_info_container'>
+                        <h2>Name</h2>
+                        <h3>Who Am I?</h3>
+                        <p>Main info</p>
+                    </div>
+                </div>
+                <div id='plant_detail_second_layer_container'>
+                    <h3>How to save me?</h3>
+                    <p>Main info</p>
+                </div>
+            </div>
         </>
     )
 }
