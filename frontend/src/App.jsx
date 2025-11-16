4b62870234a807a47201ac664af77f7d9c42f2a1
sam <samagain@sam.samaybe>
2025-11-16 23:27:59 +0100

    basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Map from './map_component/map'
 
+
 function App() {
+  const [canons,setCanons] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:5000/IUCN/1')
+      .then(res => res.json())
+      .then(data => setCanons([data]))
+      .catch(err => console.error(err));
+  }, [])
+  console.log(canons);
+
+useEffect(() => {
+  const fetchAllCanons = async () => {
+    let index = 0;
+    let allCanons = [];
+
+    while (true) {
+      try {
+        const res = await fetch(`http://localhost:5000/IUCN/${index}`);
+        if (res.status === 404) break; // no more species
+        if (!res.ok) throw new Error('Network response was not ok');
+
+        const data = await res.json();
+        allCanons.push(data);
+        index++;
+      } catch (err) {
+        console.error(err);
+        break;
+      }
+    }
+
+    setCanons(allCanons);
+  };
+
+  fetchAllCanons();
+}, []);
+
+console.log(canons);
+
 
   return (
     <>
